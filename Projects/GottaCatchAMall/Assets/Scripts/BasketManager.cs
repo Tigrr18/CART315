76a4541bdadcbeca1ef4946b939b3a5925324a34
AlexG <alexgodfroy18@gmail.com>
2026-01-22 20:13:41 -0500

    Started the basket game
    - Created project
    - Added sprites
    - Added in basket and made a script to make it move

---
+++ Projects/GottaCatchAMall/Assets/Scripts/BasketManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BasketManager : MonoBehaviour
+{
+    public float basket_x, basket_y;
+    public float basket_speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            basket_x -= basket_speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            basket_x += basket_speed;
+        }
+
+        transform.position = new Vector3(basket_x, basket_y, 0);
+    }
+
+
+}
